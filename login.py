b708f388209cb051c8021b1f35d3458a03a6eabc
mishapatel03 <mishapatel0311@gmail.com>
2020-08-12 14:10:02 +0530

    init

---
+++ login.py
@@ -0,0 +1,12 @@
+from tkinter import*
+class Login_system:
+    def __init__(self,root):
+        
+
+        self.root.title("Login System")
+
+
+
+root=Tk()
+obj=Login_system(root)
+root.mainloop()
